f6f53a006f705eb6687c744232bfaeabfafc1caa
Lasya <22b01a1284@svecw.edu.in>
2023-10-26 23:56:32 +0530

    second commit

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ import HomeAndKitchen from "./data/HomeAndKitchen.js"
 import JewelleryAccessories from "./data/JewelleryAccessories.js"
 import Kids from "./data/Kids.js"
 import Men from "./data/Men.js"
-import WomenEthnic from "./data/WomenEthnic.js"
+
 import WomenWestern from "./data/WomenWestern.js"
-
-
 
 
 let inputSearchEl = document.querySelector(".inputSearch")
@@ -31,134 +29,28 @@ inputSearchEl.addEventListener("keydown", () => {
 formInputEl.addEventListener("submit",(e)=>{
     e.preventDefault()
 
-    recentInput.push(inputSearchEl.value)
+    recentInput.unshift(inputSearchEl.value)
     console.log(recentInput)
     if(recentInput.length > 0){
         for(let  i=0;i<recentInput;i++){
             listofRecentHTMLE1+=""
         }
+        listofRecentEl.innerHTML = listofRecentHTMLE1
     }
 })
 /*function reuble*****/
-function renderSubMenu(id, data) {
-    let temp = document.getElementById(id)
-    function TempFunc() {
-        return data.map(el => {
-            let list = "";
-            list = el.data.map(element => `<p>${element}</p>`).join(" ")
-            return `
-        <div class="column">
-            <h4>${el.heading}</h4>
-            ${list}
-        </div>
-       `
-        }).join("")
-    }
-    temp.innerHTML = TempFunc()
+let womenethnic = document.getElementById(" womenethnic")
+let womenethnicHTML = " "
+console.log(WomenEthnic)
+function womenethnicFunc(){
+    WomenEthnic.map(e1 =>{
+        console.log(e1)
+    })
 }
-
-
-
-
-/****womenEthic */
-renderSubMenu("womenEthnic", WomenEthnic)
-
-/****WomenWestern */
-renderSubMenu("womenWestern", WomenWestern)
-
-//Men 
-renderSubMenu("men", Men)
-
-/***kids */
-renderSubMenu("kids", Kids)
-
-/**home % kitchen */
-renderSubMenu("HomeAndKitchen", HomeAndKitchen)
-
-/**beauty and health */
-renderSubMenu("beautyAndHealth", BeautyHealth)
-
-// Jewellery & Accessories
-renderSubMenu("JewelleryAndAccessories", JewelleryAccessories)
-
-// Bags & Footwear
-renderSubMenu("BagsFootWear", BagsFootwear)
-
-// Electronics
-renderSubMenu("Electronics", Electronics)
-
-
-
-/**********product section***************/
-import ProductData from "./meesho/data.js"
-
-const category = [...new Set(ProductData.map(el => el.category))]
-console.log(category)
-
-
-let filterData = []
-
-document.addEventListener("click", (e) => {
-
-
-    const bluetoothEl = document.getElementById("bluetooth").checked
-    const ChainsEl = document.getElementById("chains").checked
-    const KurtasEl = document.getElementById("kurtas").checked
-    const AccessoriesEl = document.getElementById("accessories").checked
-    const sareesEl = document.getElementById("sarees").checked
-    const watchEl = document.getElementById("watch").checked
-
-    console.log(bluetoothEl)
-    filterData = ProductData.filter(el => (
-        bluetoothEl && el.category == "bluetooth Headset" ||
-        ChainsEl && el.category == "Men Chains" ||
-        KurtasEl && el.category == "Kurtas" ||
-        AccessoriesEl && el.category == "Mobile Accessories" ||
-        sareesEl && el.category == "sarees" ||
-        watchEl && el.category == "watch"
-    ))
-
-    renderProductData()
-
-
-})
-
-function renderProductData(){
-    let filterDataHTML = "";
-
-    if(filterData[0]){
-        filterData.forEach(el => {
-            filterDataHTML += `
-            <div class="productCard" onclick="ClickProduct(${el.id})">
-                <div class="product_image">
-                    <img src="./meesho/productImage/${el.img}"/>
-                </div>
-                <h3 class="product_name">${el.name}</h3>
-                <p class="product_price">
-                    <span>₹</span>
-                    <span>${el.price}</span>
-                </p>
-             </div>
-            `
-        })
-    }
-    else{
-        ProductData.forEach(el => {
-            filterDataHTML += `
-            <div class="productCard" onclick="ClickProduct(${el.id})">
-                <div class="product_image">
-                    <img src="./meesho/productImage/${el.img}"/>
-                </div>
-                <h3 class="product_name">${el.name}</h3>
-                <p class="product_price">
-                    <span>₹</span>
-                    <span>${el.price}</span>
-                </p>
-             </div>
-            `
-        }) 
-    }
-    
-    document.getElementById("product_category_displayId").innerHTML = filterDataHTML
-} 
-renderProductData()
+`
+    <div>
+     <h3>All women ethnic</h3>
+     <p>view all</p>
+      </div> 
+      
+      `
